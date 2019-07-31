26a247e5933618fcbbd1935e98fa7df4c181c88f
Matt Greenfield <gmattgreenfield@gmail.com>
2019-07-31 21:26:34 +0100

    Show popup once user has changed theme 5 times

--- src/components/ThemePicker/Random/ThemePickerRandom.js
+++ src/components/ThemePicker/Random/ThemePickerRandom.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MdInvertColors } from 'react-icons/md';
+import { Link } from 'gatsby';
 
 import './ThemePickerRandom.scss';
 
@@ -7,15 +8,18 @@ class ThemePickerRandom extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // open: false,
+      clickCount: 0,
     };
 
     this.pickRandom = this.pickRandom.bind(this);
+    this.incrementCount = this.incrementCount.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.resetClickCount = this.resetClickCount.bind(this);
   }
 
-  // togglePicker() {
-  //   this.setState({ open: !this.state.open });
-  // }
+  incrementCount() {
+    this.setState({ clickCount: this.state.clickCount + 1 });
+  }
 
   pickRandom() {
     const { theme } = this.props;
@@ -31,18 +35,41 @@ class ThemePickerRandom extends React.Component {
     this.props.onThemeChange(options[randomKey]);
   }
 
+  resetClickCount() {
+    this.setState({ clickCount: 0 });
+  }
+
+  handleClick() {
+    this.incrementCount();
+    this.pickRandom();
+  }
+
   render() {
+    const { clickCount } = this.state;
+    const showPopup = clickCount > 5;
+
     return (
       <aside className="ThemePicker">
         <button
           type="button"
-          onClick={this.pickRandom}
-          aria-label="Change theme"
+          onClick={this.handleClick}
           className="ThemePicker__toggle"
         >
           <span className="ThemePicker__toggle__text">Change Theme</span>
           <MdInvertColors />
         </button>
+        {showPopup && (
+          <div className="ThemePicker__popup">
+            It looks like your enjoying the theme switcher. Why not check out
+            the{' '}
+            <Link to="/theme-switcher" onClick={this.resetClickCount}>
+              full list of themes?
+            </Link>{' '}
+            <button type="button" onClick={this.resetClickCount}>
+              No Thanks
+            </button>
+          </div>
+        )}
       </aside>
     );
   }
