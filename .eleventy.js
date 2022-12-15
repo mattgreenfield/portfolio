78c62d2f1ed4a5a2c26f64e1407bbd10fce06bc7
Matt Greenfield <gmattgreenfield@gmail.com>
2022-12-15 15:34:58 +0000

    Fix CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const CleanCSS = require("clean-css");
+const PostCSSPlugin = require("eleventy-plugin-postcss");
 
 module.exports = function (eleventyConfig) {
   // Output directory: _site
@@ -6,6 +7,9 @@ module.exports = function (eleventyConfig) {
   // Copy `img/` to `_site/img`
   //   eleventyConfig.addPassthroughCopy("img");
   //   eleventyConfig.addPassthroughCopy("css/fonts");
+
+  // uses postcss.config.js
+  eleventyConfig.addPlugin(PostCSSPlugin);
 
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
